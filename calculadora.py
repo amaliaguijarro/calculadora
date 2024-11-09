1b3d2b09c4f15821ffafd4123cbeef19cdb78987
amaliaguijarro <amaliaguijarro87@gmail.com>
2024-11-09 14:37:53 +0100

    primer commit

---
+++ calculadora.py
@@ -0,0 +1,24 @@
+
+
+numero1 = int(input("ingresar el primer numero: "))
+numero2 = int(input("ingresar el segundo numero: "))
+resta = numero1 - numero2
+print(f"la resta es: {resta}")
+
+quit(0)
+numero1 = int(input("ingresar el primer numero: "))
+numero2 = int(input("ingresar el segundo numero: "))
+suma = numero1 + numero2
+print(f"la suma es: {suma}")
+
+numero1 = int(input("ingresar el primer numero: "))
+numero2 = int(input("ingresar el segundo numero: "))
+multi = numero1 * numero2
+print(f"la multiplicacion es: {multi}")
+
+
+numero1 = int(input("ingresar el primer numero: "))
+numero2 = int(input("ingresar el segundo numero: "))
+div = numero1 / numero2
+print(f"la division es: {div}")
+
